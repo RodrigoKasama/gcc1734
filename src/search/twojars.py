a5025288b41262f1e56611a26ba80e651b8f89a0
RodrigoKasama <rodrigo.parracho@gmail.com>
2023-09-08 17:47:45 -0300

    twoJars

--- src/search/twojars.py
+++ src/search/twojars.py
@@ -22,16 +22,15 @@ class TwoJarsState:
 		Both jars ware initially empty.
 		"""
 		self.jars = (capacitys[0], capacitys[1])
-		self.initialState = (0, 0)
-		self.state = self.initialState
+		self.max_capacity = [4, 3]
 
 	def isGoal(self):
 		"""
 		  Checks to see if the pair of jars is in its goal state.
 
-				---------
-				| 2 | * |
-				---------
+						---------
+						| 2 | * |
+						---------
 
 		>>> TwoJarsState((2, 1)).isGoal()
 		True
@@ -43,6 +42,7 @@ class TwoJarsState:
 		False
 		"""
 		"*** YOUR CODE HERE ***"
+		return True if self.jars[0] == 2 else False
 		util.raiseNotDefined()
 
 	def legalMoves(self):
@@ -64,21 +64,25 @@ class TwoJarsState:
 		['fillJ4', 'pourJ3intoJ4', 'emptyJ3', 'emptyJ4']
 		"""
 		"*** YOUR CODE HERE ***"
+
 		legalActions = []
+		j4, j3 = self.jars
 
-		j4, j3 = self.state
+		if j4 == 0:
+			legalActions.extend(["fillJ4"])
+		elif j4 == self.max_capacity[0]:
+			legalActions.extend(["pourJ4intoJ3", "emptyJ4"])
+		else:
+			legalActions.extend(["fillJ4", "pourJ4intoJ3", "emptyJ4"])
+
 		
-		if j4 != 0:
-			legalActions.append("fillJ4", "pourJ4intoJ3", "emptyJ4")
-		if j3 != 0:
-			legalActions.append("fillJ3", "pourJ3intoJ4", "emptyJ3")
+		if j3 == 0:
+			legalActions.extend(["fillJ3"])
+		elif j3 == self.max_capacity[1]:
+			legalActions.extend(["pourJ3intoJ4", "emptyJ3"])
+		else:
+			legalActions.extend(["fillJ3", "pourJ3intoJ4", "emptyJ3"])
 
-		if j4 == self.jars[0]:
-			legalActions.remove("fillJ4")
-		if j3 == self.jars[1]:
-			legalActions.remove("fillJ3")
-   
-		print(legalActions)
 		return legalActions
 		util.raiseNotDefined()
 
@@ -95,47 +99,52 @@ class TwoJarsState:
 		"""
 		"*** YOUR CODE HERE ***"
 
-		curr_state = self.state
-		resultState = 0
+		# resultState = 0
+
 		if move in ["emptyJ3", "emptyJ4"]:
 			if move == "emptyJ3":
-				print("Esvaziar j3")
-				resultState = TwoJarsState((curr_state[0], 0))
+				# print("Esvaziar j3")
+				resultState = TwoJarsState((self.jars[0], 0))
 			else:
-				print("Esvaziar j4")
-				resultState = TwoJarsState((0, curr_state[1]))
+				# print("Esvaziar j4")
+				resultState = TwoJarsState((0, self.jars[1]))
 
 		elif move in ["pourJ3intoJ4", "pourJ4intoJ3"]:
+			sum_vol = sum(self.jars)
 			if move == "pourJ3intoJ4":
-				print("j3 -> j4")
-				resultState = TwoJarsState((sum(curr_state) % self.jars[0], 0))
+				# print(self.jars, min(sum_vol, self.max_capacity[0]))
+				resultState = TwoJarsState((min(sum_vol, self.max_capacity[0]), 0))
+				# print("j4 <- j3")
 			else:
-				resultState = TwoJarsState((0, sum(curr_state) % self.jars[1]))
-				print("j4 -> j3")
+				resultState = TwoJarsState((0, min(sum_vol, self.max_capacity[1])))
+				# print("j4 -> j3")
 
 		else:
 			if move == "fillJ3":
-				resultState = TwoJarsState((curr_state[0], self.jars[1]))
-				print("Fill j3")
+				resultState = TwoJarsState((self.jars[0], self.max_capacity[1]))
+				# print("Fill j3")
 			else:
-				resultState = TwoJarsState((self.jars[0], curr_state[1]))
-				print("Fill j4")
-				
-		return resultState if resultState != 0 else util.raiseNotDefined()
+				# print(move)
+				resultState = TwoJarsState((self.max_capacity[0], self.jars[1]))
+				# print("Fill j4")
+
+		return resultState
 		util.raiseNotDefined()
 
 	# Utilities for comparison and display
 	def __eq__(self, other):
 		"""
-				Overloads '==' such that two pairs of jars with the same volume of water
-		  are equal.
+						Overloads '==' such that two pairs of jars with the same volume of water
+		  are equal. ['fillJ4', 'pourJ4intoJ3', 'emptyJ4', 'fillJ3']
 
 		  >>> TwoJarsState((0, 1)) == \
-				  TwoJarsState((1, 0)).result('left')
+						  TwoJarsState((1, 0)).result('left')
 		  True
 		"""
 		"*** YOUR CODE HERE ***"
-		util.raiseNotDefined()
+		# print(other)
+		return True if ((self.jars[0] == other.jars[0]) and (self.jars[1] == other.jars[1])) else False
+		# util.raiseNotDefined()
 
 	def __hash__(self):
 		return hash(str(self.jars))
@@ -145,63 +154,61 @@ class TwoJarsState:
 		  Returns a display string for the maze
 		"""
 		"*** YOUR CODE HERE ***"
-		util.raiseNotDefined()
+		return str(self.jars)
 
 	def __str__(self):
 		return self.__getAsciiString()
 
 
 class TwoJarsSearchProblem(search.SearchProblem):
-	"""
-	  Implementation of a SearchProblem for the Two Jars domain
+    """
+      Implementation of a SearchProblem for the Two Jars domain
 
-	  Each state is represented by an instance of an TwoJarsState.
-	"""
+      Each state is represented by an instance of an TwoJarsState.
+    """
+    def __init__(self, start_state):
+        "Creates a new TwoJarsSearchProblem which stores search information."
+        self.start_state = start_state
 
-	def __init__(self, start_state):
-		"Creates a new TwoJarsSearchProblem which stores search information."
-		self.start_state = start_state
+    def getStartState(self):
+        return self.start_state
 
-	def getStartState(self):
-		return self.start_state
+    def isGoalState(self,state):
+        return state.isGoal()
 
-	def isGoalState(self, state):
-		return state.isGoal()
+    def expand(self,state):
+        """
+          Returns list of (child, action, stepCost) pairs where
+          each child is either left, right, up, or down
+          from the original state and the cost is 1.0 for each
+        """
+        child = []
+        for a in self.getActions(state):
+            next_state = self.getNextState(state, a)
+            child.append((next_state, a, self.getActionCost(state, a, next_state)))
+        return child
 
-	def expand(self, state):
-		"""
-		  Returns list of (child, action, stepCost) pairs where
-		  each child is either left, right, up, or down
-		  from the original state and the cost is 1.0 for each
-		"""
-		child = []
-		for a in self.getActions(state):
-			next_state = self.getNextState(state, a)
-			child.append(
-				(next_state, a, self.getActionCost(state, a, next_state)))
-		return child
+    def getActions(self, state):
+        return state.legalMoves()
 
-	def getActions(self, state):
-		return state.legalMoves()
+    def getActionCost(self, state, action, next_state):
+        assert next_state == state.result(action), (
+            "getActionCost() called on incorrect next state.")
+        return 1
 
-	def getActionCost(self, state, action, next_state):
-		assert next_state == state.result(action), (
-			"getActionCost() called on incorrect next state.")
-		return 1
+    def getNextState(self, state, action):
+        assert action in state.legalMoves(), (
+            "getNextState() called on incorrect action")
+        return state.result(action)
 
-	def getNextState(self, state, action):
-		assert action in state.legalMoves(), (
-			"getNextState() called on incorrect action")
-		return state.result(action)
+    def getCostOfActionSequence(self, actions):
+        """
+         actions: A list of actions to take
 
-	def getCostOfActionSequence(self, actions):
-		"""
-		 actions: A list of actions to take
-
-		This method returns the total cost of a particular sequence of actions.  The sequence must
-		be composed of legal moves
-		"""
-		return len(actions)
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        return len(actions)
 
 
 def createRandomTwoJarsState(moves=10):
@@ -212,7 +219,9 @@ def createRandomTwoJarsState(moves=10):
 	  of 'moves' random moves to a solved state.
 	"""
 	volume_of_J3 = randint(0, 4)
-	a_state = TwoJarsState((2, volume_of_J3))
+
+	a_state = TwoJarsState((0, volume_of_J3))
+
 	for i in range(moves):
 		# Execute a random legal move
 		a_state = a_state.result(random.sample(a_state.legalMoves(), 1)[0])
@@ -220,15 +229,29 @@ def createRandomTwoJarsState(moves=10):
 
 
 def main():
+
 	start_state = createRandomTwoJarsState(8)
-	print('A random initial state:')
+
+	# a = TwoJarsState((1,0))
+	# b = TwoJarsState((1,0))
+	# c = TwoJarsState((0,1))
+
+	# print(a == b, a == c, b == c)
+
+	start_state = TwoJarsState((0,0))
+	print('A random initial state:', start_state)
+
+	print("Ações possiveis: ", start_state.legalMoves())
+	print(str(start_state.result("fillJ3")))
+
 	print(start_state)
-
-	return
-
+	# return
 	problem = TwoJarsSearchProblem(start_state)
 	path = search.breadthFirstSearch(problem)
+	# print(path)
+
 	print('BFS found a path of %d moves: %s' % (len(path), str(path)))
+
 	curr = start_state
 	i = 1
 	for a in path:
@@ -238,6 +261,7 @@ def main():
 
 		input("Press return for the next state...")   # wait for key stroke
 		i += 1
+	return
 
 
 if __name__ == '__main__':
